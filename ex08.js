e375f95cd7538a4051270e7bd0ba5330b21023bc
JORGE ALEXANDRE MAIWORM <jrmaiworm@hotmail.com>
2021-05-20 17:39:58 -0300

    commit

---
+++ ex08.js
@@ -0,0 +1,9 @@
+var vel = 40
+console.log(`Seu veiculo está à ${vel} KM/H.`)
+if (vel > 50) {
+    console.log("Seu veiculo foi multado!")
+}
+
+
+
+console.log(`Dirija com cuidado, use sempre o cinto de segurança!`)
---
+++ ex09.js
@@ -0,0 +1,8 @@
+var pais = 'EUA'
+
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasileiro') {
+    console.log('Você é nacional!')
+} else {
+    console.log('Você é Internacional!')
+}
