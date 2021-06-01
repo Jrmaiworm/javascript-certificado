b932298ac0bd653e113758740d451f31b2e8d64e
JORGE ALEXANDRE MAIWORM <jrmaiworm@hotmail.com>
2021-06-01 18:47:32 -0300

    up

---
+++ ex11.js
@@ -0,0 +1,14 @@
+var idade = 22
+
+ if (idade<16){
+    console.log("nao vota")
+}
+ else if(idade>16){
+    console.log('voto facultativo')
+}
+else (idade>18){
+    console.log('voto obrigatorio')
+}
+
+
+
