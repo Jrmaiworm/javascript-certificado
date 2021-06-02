dca9e27442ee1223b3a1635aade49f4e520216eb
JORGE ALEXANDRE MAIWORM <jrmaiworm@hotmail.com>
2021-06-02 09:55:06 -0300

    up

--- exercicios site/script.js
+++ exercicios site/script.js
@@ -1,1 +1,19 @@
-alert( "ola!")
+
+
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = (`Agora são ${hora} horas.`)
+    if(hora >= 0 && hora<12){
+//bom dia//
+        img.src ='imagens/manha.png'
+    }else if(hora >=12 && hora<=18){
+    //boa tarde//
+img.src='imagens/tarde.png'
+} else {
+    //boa noite
+    img.src='imagens/noite.png'
+}
+}
